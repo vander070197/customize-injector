8767c6b33096cf8c211769a31ddf11694f8a2b3a
vander070197 <reyvandersanchez@gmail.com>
2025-02-08 01:52:15 +0800

    Update script.js

--- website-custome/script.js
+++ website-custome/script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function() {
     // Get the current URL
     var currentUrl = window.location.href;
 
@@ -7,7 +7,38 @@ window.onload = function () {
      
         
     }
-alert(true);
-    document.body.innerHTML += '<div class="custom_sidebar"> <div class="Custsidebar_con"> </div> </div>';
+    
+  alert(true)
+document.body.innerHTML += `
+<div class="custom_sidebar" id="sidebar">
+    <div class="Custsidebar_con">
 
-};
+        <div class="group_btn">
+           <div class="permalink_btn">
+            <button>Check Permalink</button>
+           </div>
+
+            <div class="validationBtn">
+                <button class="html_btn">HTML Validation</button>
+                <button class="css_btn">CSS Validation</button>
+            </div>
+        </div>
+        
+    </div>
+</div>`;
+
+//slide sidebar
+document.addEventListener("keydown", function (event) {
+    if (event.altKey && event.key.toLowerCase() === 'u') {
+        event.preventDefault(); // Prevent default browser action
+        toggleSidebar();
+    }
+});
+
+function toggleSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("show"); // Toggle sidebar class
+}
+
+
+});
