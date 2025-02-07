941d640854a12e274085c5689631072f848014b9
vander070197 <reyvandersanchez@gmail.com>
2025-02-07 21:23:35 +0800

    Update script.js

--- website-custome/script.js
+++ website-custome/script.js
@@ -1,1 +1,13 @@
+window.onload = function () {
+    // Get the current URL
+    var currentUrl = window.location.href;
 
+    // Check if the URL contains "sitemap"
+    if (currentUrl.includes("/sitemap")) {
+     
+        
+    }
+alert(true);
+    document.body.innerHTML += '<div class="custom_sidebar"> <div class="Custsidebar_con"> </div> </div>';
+
+};
